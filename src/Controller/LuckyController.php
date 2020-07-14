1b2f3f834b7985bd5785ea661eadf4b872707e06
elena100880 <100880kr@gmail.com>
2020-07-14 21:20:25 +0200

    peredel w template lucky

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,15 +3,28 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class LuckyController
+
+class LuckyController extends AbstractController
 {
-    public function number(): Response
+    public function number()
     {
+        
         $number = random_int(0, 100);
+       
+        $contents = $this->renderView('lucky/index.html.twig',
+       
+            [
+                'random_strings' => ['ala', 'ma','kot', 'pies'],
+                'number'=>$number,
+            ],
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
+        
         );
+
+        return new Response($contents);
     }
+     
+   
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,23 +9,17 @@ class ProductController extends AbstractController
 {
     public function index()
     {
-        // ...
-
-        // the `render()` method returns a `Response` object with the
-        // contents created by the template
-      //  return $this->render('product/index.html.twig', [
-       //     'category' => '...',
-        //    'promotions' => ['...', '...'],
-       // ]);
-
-        // the `renderView()` method only returns the contents created by the
-        // template, so you can use those contents later in a `Response` object
+        
+        $a=32;
+       
         $contents = $this->renderView('product/index.html.twig',
        
-        [
-            'random_strings' => ['ala', 'ma','kot', 'pies']
-        ]
+            [
+                'random_strings' => ['ala', 'ma','kot', 'pies'],
+                'www'=>$a
+            ],
 
+        
         );
 
         return new Response($contents);
