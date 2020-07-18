120e4d3c75aa574a957c44f3c453919579f65611
elena100880 <100880kr@gmail.com>
2020-07-18 22:21:28 +0200

    form_registration

---
+++ src/Controller/RegistController.php
@@ -0,0 +1,69 @@
+<?php
+// src/Controller/RegistController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class RegistController extends AbstractController
+{
+
+    public function registration (Request $request)
+    {
+        //формирует конфиг внутри себя по переданным тобой вещам и возвращает форму
+        $defaultData = [];
+        $form = $this->createFormBuilder($defaultData)
+            ->add('Enter_your_email', EmailType::class)
+            ->add('Enter_your_password', PasswordType::class)
+            ->add('Repeat_password', PasswordType::class)
+            ->add('SEND', SubmitType::class)
+            ->add('RESET', ResetType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            
+            $data = $form->getData();
+
+            if ($data['Enter_your_password']!=$data['Repeat_password']) {
+                $message='Enter equal passwords';      
+                $contents = $this->renderView('registration/index.html.twig',
+                    [
+                        'data'=> $data,
+                        'form' => $form->createView(),
+                        'message'=> $message
+                    ],
+                );
+            }
+            else {
+                $message='OK';      
+                $contents = $this->renderView('registration/index.html.twig',
+                    [
+                        'data'=> $data,
+                        'form' => $form->createView(),
+                        'message'=> $message
+                    ],
+                );
+
+            }
+        }
+        else {
+            $message="";
+            $contents = $this->renderView('registration/index.html.twig',
+                [
+                     'form' => $form->createView(),
+                    'message'=> $message
+                ],
+            );
+        }
+
+        return new Response($contents);
+        
+    }
+}
