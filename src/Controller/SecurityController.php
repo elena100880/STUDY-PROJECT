fb264eb33acfcd3a7c932139de919cd3e0818221
elena100880 <100880kr@gmail.com>
2020-08-21 10:46:49 +0200

    logging in

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,16 +1,29 @@
 <?php
 
-//App/Controller/SecurityController.php
-
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 
 class SecurityController extends AbstractController
 {
-    
-    public function logout():void
+    public function login(Request $request, AuthenticationUtils $authenticationUtils)
     {
-        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        $contents = $this->renderView('security/login.html.twig',
+            [
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            ],
+        );
+        return new Response($contents);
     }
 }
